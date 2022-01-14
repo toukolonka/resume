4cae0443e598c7ba917fbff3068e29851d6fadfc
Touko Lonka <touko.lonka@solita.fi>
2022-01-14 13:51:28 +0200

    Add greeting

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,7 +4,12 @@ import './Intro.css';
 function Intro() {
   return (
     <div className="intro">
-      <div className="intro-left">left</div>
+      <div className="intro-left">
+        <div className="intro-left-wrapper">
+          <h2 className="intro-intro">Hello, my name is</h2>
+          <h1 className="intro-name">Touko</h1>
+        </div>
+      </div>
       <div className="intro-right">right</div>
     </div>
   );

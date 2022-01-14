568fc2c642ef1b112d1ba006f08aa0baa75cf92d
Touko Lonka <touko.lonka@solita.fi>
2022-01-14 16:29:24 +0200

    Finish Intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Intro from './components/intro/Intro';
+import Intro from './components/Intro/Intro';
 
 function App() {
   return <div><Intro /></div>;
--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Me from '../../img/me.png'
 import './Intro.css';
 
 function Intro() {
@@ -7,10 +8,86 @@ function Intro() {
       <div className="intro-left">
         <div className="intro-left-wrapper">
           <h2 className="intro-intro">Hello, my name is</h2>
-          <h1 className="intro-name">Touko</h1>
+          <h1 className="intro-name">Touko Lonka</h1>
+          <div className="intro-title">
+            <div className="intro-title-wrapper">
+              <div className="intro-title-item">Web Developer</div>
+              <div className="intro-title-item">Software Designer</div>
+              <div className="intro-title-item">Programmer</div>
+              <div className="intro-title-item">UI/UX Designer</div>
+              <div className="intro-title-item">University Student</div>
+            </div>
+          </div>
+            <p className="intro-desc">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur
+            </p>
         </div>
+        <svg
+          width="75"
+          height="75"
+          viewBox="0 0 75 75"
+          fill="none"
+          stroke="black"
+          className="intro-scroll"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="scroll">
+            <path
+              id="Vector"
+              d="M40.5 15L34.5 9L28.5 15"
+              stroke-width="3"
+              stroke-miterlimit="10"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              id="Vector_2"
+              d="M28.5 24L34.5 30L40.5 24"
+              stroke-width="3"
+              stroke-miterlimit="10"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <g id="Group">
+              <path
+                id="Vector_3"
+                d="M9 37.5H60"
+                stroke-width="3"
+                stroke-miterlimit="10"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+            <path
+              id="Vector_4"
+              d="M34.5 27V9"
+              stroke-width="2.9895"
+              stroke-miterlimit="10"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <g id="Group_2">
+              <path
+                id="Vector_5"
+                d="M9 27C9 12.918 20.418 1.5 34.5 1.5C48.5859 1.5 60 12.918 60 27C60 29.8906 60 45.1094 60 48C60 62.082 48.5859 73.5 34.5 73.5C20.418 73.5 9 62.082 9 48C9 45.1094 9 29.8906 9 27Z"
+                stroke-width="3"
+                stroke-miterlimit="10"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+          </g>
+        </svg>
       </div>
-      <div className="intro-right">right</div>
+      <div className="intro-right">
+        <div className="intro-bg"></div>
+        <img src={Me} alt="" className="intro-image" />
+      </div>
     </div>
   );
 }
